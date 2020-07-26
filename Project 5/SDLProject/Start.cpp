b7f97ec2524192abf5bda61f77bbf98efa61a42a
PingChang1999 <47250629+PingChang1999@users.noreply.github.com>
2020-07-26 04:05:43 -0400

    Adding Project 5

---
+++ Project 5/SDLProject/Start.cpp
@@ -0,0 +1,17 @@
+#include "Start.h"
+
+void Start::Initialize() {
+	state.nextScene = -1;
+}
+
+void Start::Update(float deltaTime) {
+
+}
+
+void Start::Render(ShaderProgram* program) {
+
+}
+
+void Start::Reset() {
+
+}
