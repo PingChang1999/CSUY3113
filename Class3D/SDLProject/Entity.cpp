330e97ffdfc6f77175d34a72e5d712c1c62865b7
PingChang1999 <47250629+PingChang1999@users.noreply.github.com>
2020-07-30 22:13:46 -0400

    Adding Class3D

---
+++ Class3D/SDLProject/Entity.cpp
@@ -0,0 +1,47 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+    position = glm::vec3(0);
+    acceleration = glm::vec3(0);
+    velocity = glm::vec3(0);
+    rotation = glm::vec3(0);
+
+    modelMatrix = glm::mat4(1.0f);
+    
+    speed = 0.0f;
+}
+
+void Entity::Update(float deltaTime)
+{
+    velocity += acceleration * deltaTime;
+    position += velocity * deltaTime;
+
+    if (entityType = CUBE) {
+        rotation.y += 45 * deltaTime;
+        rotation.z += 45 * deltaTime;
+    }
+    
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.z), glm::vec3(1.0f, 0.0f, 1.0f));
+}
+
+void Entity::Render(ShaderProgram *program) {
+    program->SetModelMatrix(modelMatrix);
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glVertexAttribPointer(program->positionAttribute, 3, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+    
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+    
+    glDrawArrays(GL_TRIANGLES, 0, numVertices);
+    
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
